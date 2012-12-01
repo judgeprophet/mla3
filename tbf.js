149a91d5d51b878a59e2f91b708a27188fc1df63
Sebastien Vincent <judgeprophet@gmail.com>
2012-12-01 11:34:58 -0500

    Lets Go

--- tbf.js
+++ tbf.js
@@ -1,16 +1,14 @@
-var t;
-
     // Define the entry point
     $(document).ready(function()
     {
 
-		// Get 3 users
-		jQuery.ajax({
-		  'url':'http://api.tum.bz/v1/users?limit=3&apikey=E2g2K9GvzBSvH6coGFOFl3ASLL5qjSgg',
-		  'dataType':'jsonp',
-		  'jsonp': 'callback',
-		  'jsonpCallback': 'jsonpcallback'
-		});//.success(function(data){
+		// // Get 3 users
+		// jQuery.ajax({
+		//   'url':'http://api.tum.bz/v1/users?limit=10&apikey=E2g2K9GvzBSvH6coGFOFl3ASLL5qjSgg',
+		//   'dataType':'jsonp',
+		//   'jsonp': 'callback',
+		//   'jsonpCallback': 'jsonpcallback'
+		// });//.success(function(data){
 
 		//console.log(data[0])
 		//$('#result').html(data[0].toString());
@@ -22,23 +20,61 @@ var t;
     
     function jsonpcallback(data)
     {
-		t = data[0].id;
-		$('#result').html(data[0].username);
+    	console.log(data);
     }
 
-alert(click());
-
-function click()
+function getPlayer(callback)
 {
 
-	jQuery.ajax({
-		  'url':'http://api.tum.bz/v1/users?limit=3&apikey=E2g2K9GvzBSvH6coGFOFl3ASLL5qjSgg',
-		  'dataType':'jsonp',
-		  async: false
+		jQuery.ajax({
+		  'url':'http://api.tum.bz/v1/users/judgeprophet?apikey=E2g2K9GvzBSvH6coGFOFl3ASLL5qjSgg',
+		  'dataType':'jsonp'
 		}).success(function(data)
 		{
-		return data[0].id
-		//$('#result').html(data[0].toString());
-		//$('#result').html(data[0].username);
+			callback(data);
 		});
+}
 
+function getOpponent(callback)
+{
+
+		jQuery.ajax({
+
+		  'url':'http://api.tum.bz/v1/users?respecting=judgeprophet&limit=100&apikey=E2g2K9GvzBSvH6coGFOFl3ASLL5qjSgg',
+		  'dataType':'jsonp'
+		}).success(function(data)
+		{
+			callback(data);
+		});
+}
+
+
+
+
+function getPlayerStats(callback)
+{
+
+		jQuery.ajax({
+		  'url':'http://api.tum.bz/v1/reviews?user=judgeprophet&limit=200&apikey=E2g2K9GvzBSvH6coGFOFl3ASLL5qjSgg',
+		  'dataType':'jsonp'
+		}).success(function(data)
+		{
+			console.log(data.length);
+			//callback(data);
+		});
+}
+
+
+function getOpponnent()
+{
+
+
+	
+ return "Opponnent";
+}
+
+function getAlternateArena()
+{
+	
+ return "Opponnent";
+}
